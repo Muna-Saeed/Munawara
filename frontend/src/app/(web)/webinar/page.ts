06372dc8e1c5c0e523a93e44b8ae95f9629d9daf
abdiwoli <abdiwolih@gmail.com>
2025-07-03 15:09:17 +0300

    update form link

--- frontend/src/app/(web)/webinar/page.ts
+++ frontend/src/app/(web)/webinar/page.ts
@@ -2,5 +2,5 @@
 import { redirect } from 'next/navigation';
 
 export default function RedirectToForm() {
-    redirect('https://docs.google.com/forms/d/e/1FAIpQLScI4A5j50eCKdTn3FPl9VxreHgOuRp4Wk3SupE_ltr8u70gMQ/viewform');
+    redirect('https://forms.gle/caRidvmP7MCotZaX9');
 }
